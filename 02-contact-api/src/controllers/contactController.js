f199b7e291633bef4d7d0724ba2e061f462051ad
Burak Karaman <karaman.buraak@gmail.com>
2024-06-12 19:43:14 +0200

    Refactor contactController.js:  add get, update, and delete methods

--- 02-contact-api/src/controllers/contactController.js
+++ 02-contact-api/src/controllers/contactController.js
@@ -8,20 +8,33 @@ module.exports = {
     // const data = await sequelize.query("SELECT * FROM contacts"); //* klasik sql sorgularin yerine ORM kullaniyoruz.
     // const data = await Contact.findAndCountAll();
     // const data = await Contact.findAll();
-    try {
-      const data = await Contact.findAll({
-        where: {
-          isActive: true,
-        },
-      });
-      // throw new Error("Hata firlattim, yakala!");
-      res.status(200).send({
-        error: false,
-        contacts: datas,
-      });
-    } catch (error) {
-      next(error);
-    }
+
+    //   try {
+    //     const data = await Contact.findAll({
+    //       where: {
+    //         isActive: true,
+    //       },
+    //     });
+    //     // throw new Error("Hata firlattim, yakala!");
+    //     res.status(200).send({
+    //       error: false,
+    //       contacts: datas,
+    //     });
+    //   } catch (error) {
+    //     next(error);
+    //   }
+    // },
+
+    const data = await Contact.findAll({
+      where: {
+        isActive: true,
+      },
+    });
+    // throw new Error("Hata firlattim, yakala!");
+    res.status(200).send({
+      error: false,
+      contacts: data,
+    });
   },
   create: async (req, res) => {
     //* SQL (Klasik sql sorgusu) 1. eski ve uzun yol
@@ -49,7 +62,53 @@ module.exports = {
       contact: data,
     });
   },
-  get: async (req, res) => {},
-  update: async (req, res) => {},
-  delete: async (req, res) => {},
+  get: async (req, res) => {
+    const contact = await Contact.findByPk(req.params.id);
+    if (contact) {
+      res.status(200).send({
+        error: false,
+        contact,
+      });
+    } else {
+      res.errorStatusCode = 404;
+      throw new Error("Istenilen contact bilgisi bulunmadi!", {
+        cause: `Gönderilen contact id bilgisi : ${req.params.id}`,
+      });
+    }
+  },
+  update: async (req, res) => {
+    const contact = await Contact.update(req.body, {
+      where: { id: req.params.id },
+    });
+
+    let updatedContact;
+    if (contact == 1) {
+      updatedContact = await Contact.findByPk(req.params.id);
+      res.status(201).send({
+        error: false,
+        contact,
+        updatedContact,
+      });
+    } else {
+      res.errorStatusCode = 404;
+      throw new Error("Istenilen contact bilgisi bulunmadi!", {
+        cause: `Gönderilen contact id bilgisi : ${req.params.id}`,
+      });
+    }
+  },
+
+  delete: async (req, res) => {
+    const contact = await Contact.destroy({ where: { id: req.params.id } });
+    if (contact) {
+      res.status(204).send({
+        error: false,
+        contact,
+      });
+    } else {
+      res.errorStatusCode = 404;
+      throw new Error("Istenilen contact bilgisi bulunmadi!", {
+        cause: `Gönderilen contact id bilgisi : ${req.params.id}`,
+      });
+    }
+  },
 };
