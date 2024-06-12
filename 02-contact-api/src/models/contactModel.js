d7e8bb4a0f3f6efbbc597c4b4a2386a69c4091a8
Burak Karaman <karaman.buraak@gmail.com>
2024-06-12 16:11:54 +0200

    Create contactModel.js with a define Contact model and sync the database

--- 02-contact-api/src/models/contactModel.js
+++ 02-contact-api/src/models/contactModel.js
@@ -1,1 +1,34 @@
 //* Contact Model
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../configs/db");
+
+const Contact = sequelize.define("contacts", {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  address: {
+    type: DataTypes.TEXT,
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+});
+
+sequelize.sync();
+
+module.exports = Contact;
