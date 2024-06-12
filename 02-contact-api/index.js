fbb05a17bec3b440ca526283dcfbfdbecbb9e838
Burak Karaman <karaman.buraak@gmail.com>
2024-06-12 18:57:44 +0200

    Add express-async-errors, error handler middleware

--- 02-contact-api/index.js
+++ 02-contact-api/index.js
@@ -1,5 +1,6 @@
 "use strict";
 const express = require("express");
+require("express-async-errors");
 const app = express();
 app.use(express.json());
 
@@ -9,5 +10,6 @@ app.all("/", (req, res) => {
 
 // ROUTER
 app.use(require("./src/routers/contactRouter"));
+app.use(require("./src/middlewares/errorHandler"));
 
 app.listen(8000, () => console.log(`server runned http://localhost:8000`));
--- 02-contact-api/src/controllers/contactController.js
+++ 02-contact-api/src/controllers/contactController.js
@@ -1,14 +1,48 @@
 "use strict";
 const Contact = require("../models/contactModel");
+const { sequelize } = require("../configs/db");
+const { trace } = require("../routers/contactRouter");
+
 module.exports = {
-  list: async (req, res) => {
-    const data = await Contact.findAll();
-    res.status(200).send({
-      error: false,
-      contacts: data,
-    });
+  list: async (req, res, next) => {
+    // const data = await sequelize.query("SELECT * FROM contacts"); //* klasik sql sorgularin yerine ORM kullaniyoruz.
+    // const data = await Contact.findAndCountAll();
+    // const data = await Contact.findAll();
+    try {
+      const data = await Contact.findAll({
+        where: {
+          isActive: true,
+        },
+      });
+      // throw new Error("Hata firlattim, yakala!");
+      res.status(200).send({
+        error: false,
+        contacts: datas,
+      });
+    } catch (error) {
+      next(error);
+    }
   },
   create: async (req, res) => {
+    //* SQL (Klasik sql sorgusu) 1. eski ve uzun yol
+    // const { firstName, lastName, email, phone, address, isActive } = req.body;
+    // const data = await sequelize.query(
+    //   "INSERT INTO contacts (firstName, lastName, email, phone, address, createdAt, updatedAt, isActive) VALUES (?,?,?,?,?,?,?,?)",
+    //   {
+    //     replacements: [
+    //       firstName,
+    //       lastName,
+    //       email,
+    //       phone,
+    //       address,
+    //       new Date(),
+    //       new Date(),
+    //       isActive || true,
+    //     ],
+    //   }
+    // );
+
+    //* Sequelize (ORM) 2. kisa ve yeni yol
     const data = await Contact.create(req.body);
     res.status(201).send({
       error: false,
